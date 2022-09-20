e4da5d520a9c255435f8f40db865ab3913a372f7
camila <camilamaya9619@gmail.com>
2022-09-20 11:12:34 -0500

    creacion de proyecto mascotas_MVC

---
+++ src/mascotas_mvc/Mascotas_MVC.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mascotas_mvc;
+
+/**
+ *
+ * @author giova
+ */
+public class Mascotas_MVC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
